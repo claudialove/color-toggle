531e755b62fae16cb5302eeba706adcb0f6f509f
Claudia Petty <claudialove32@gmail.com>
2020-04-08 11:11:59 -0400

    first code push

---
+++ color-toggle.js
@@ -0,0 +1,11 @@
+//color changer
+
+//select
+var button = document.querySelector("button");
+
+//manipulate
+button.addEventListener("click", function(){
+    document.body.classList.toggle("bisque");
+    });
+  
+    
